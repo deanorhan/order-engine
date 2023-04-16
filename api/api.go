141420f534648d74c63ce65fc74b77effbe2f595
Dean <deanorhan@gmail.com>
2023-04-16 15:46:50 -0700

    starting dev

---
+++ api/api.go
@@ -0,0 +1,7 @@
+package api
+
+import "github.com/labstack/echo"
+
+func Order(e *echo.Echo) {
+
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"github.com/deanorhan/order-engine/api"
+	"github.com/labstack/echo"
+)
 
 func main() {
 	e := echo.New()
+
+	api.Order(e)
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
